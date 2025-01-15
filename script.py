997ec3cb91fa04462db246b5b75d427a852aa626
Diego Santos <diego.santos@2rpnet.com>
2025-01-15 10:52:34 -0300

    feat: iimplementation of python scripts for kubeflow and docker-compose file to upload the container

--- script.py
+++ script.py
@@ -1,15 +1,14 @@
 from kfp import dsl
 import kfp.compiler as compiler
+import subprocess
 
 @dsl.component(base_image="python:3.12")
 def etapa_gerar_dados():
-    import subprocess
-    subprocess.run(["jupyter", "nbconvert", "--execute", "--inplace", "notebook1_pre_processamento.ipynb"])
+    subprocess.run(["jupyter", "nbconvert", "--execute", "--inplace", "notebook1.ipynb"])
 
 @dsl.component(base_image="python:3.12")
 def etapa_treinamento():
-    import subprocess
-    subprocess.run(["jupyter", "nbconvert", "--execute", "--inplace", "notebook2_treinamento.ipynb"])
+    subprocess.run(["jupyter", "nbconvert", "--execute", "--inplace", "notebook2.ipynb"])
 
 @dsl.pipeline(name="pipeline-teste-ml")
 def pipeline_teste_ml():
